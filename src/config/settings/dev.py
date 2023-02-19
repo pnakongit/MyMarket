b0eea7c0c05d47a44dc53a4548109344442148b9
pnakongit <pnakongithub@gmail.com>
2023-02-19 20:14:20 +0200

    Configurated new project

---
+++ src/config/settings/dev.py
@@ -0,0 +1,16 @@
+from config.settings.base import *  # NOQA
+
+DEBUG = True
+
+SECRET_KEY = "django-insecure-s4hy4e0bscfl)objduu9@*!889_7ewf$e!)ganyn!r(zx(me$*"
+
+ALLOWED_HOSTS = []
+
+STATIC_URL = "static/"
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
